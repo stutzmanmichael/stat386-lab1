12b4105cd550f34cae08255df05a5967fd33f0f8
Michael <stutzmanmichael90@gmail.com>
2025-09-19 13:40:50 -0600

    Add custom print function

--- github_issue_page_demo/main.py
+++ github_issue_page_demo/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from github-issue-page-demo!")
+from utils import print_custom
 
+def main():
+    print("Hello, GitHub!")
+    print_custom("This is a custom message from the print_custom module. You did it right.")
 
 if __name__ == "__main__":
     main()
